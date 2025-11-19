eaba18ec2182ae24e26c09884675cf829ae3e581
Lucas Rappatta <233995129+JackFlack72@users.noreply.github.com>
2025-11-19 13:10:39 -0600

    nested queries exercise

---
+++ nested-queries.sql
@@ -0,0 +1,39 @@
+use northwind;
+
+/* 1. What is the product name(s) of the most expensive products? 
+      HINT: Find the max price in a subquery and then use that value to find products whose
+      price equals that value.
+*/
+select productname
+from products
+where unitprice = (select max(unitprice)
+				   from products
+				   order by productname);
+
+/* 2. What is the order id, shipping name and shipping address of all orders
+	  shipped via "Federal Shipping"? HINT: Find the shipper id of "Federal
+	  Shipping" in a subquery and then use that value to find the orders that
+	  used that shipper.
+*/
+select orderid, shipname, shipaddress
+from orders
+where shipvia in (select shipperid
+				   from shippers
+                   where companyname like "%federal shipping%");
+
+/* 3. What are the order ids of the orders that ordered "Sasquatch Ale"? HINT:
+	  Find the product id of "Sasquatch Ale" in a subquery and then use that
+	  value to find the matching orders from the `order details` table. Because
+	  the `order details` table has a space in its name, you will need to surround
+	  it with back ticks in the FROM clause.
+*/
+select orderid
+from `order details`
+where productid in (select productid
+				  from products
+				  where productname like "sasquach ale");
+
+#  4. What is the name of the employee that sold order 10266?
+
+
+#  5. What is the name of the customer that bought order 10266?
